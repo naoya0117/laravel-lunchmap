89956aaa06dc782aef8553ed463c96cb79384367
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2023-12-09 21:17:46 +0900

    カラムを新しく追加した

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Shop extends Model
 {
     use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
 }
