e2b7751edcbdd2cbb156a575edf7c70805ad6cce
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2023-12-10 09:31:44 +0900

    レイアウトの共通部分をlayoutとして集約

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset='UTF-8'>
-        <title>Lunchmap</title>
-        <style>body {padding: 10px;}</style>
-    </head>
-    <body>
-        <h1>お店一覧</h1>
+@extends('layout')
+@section('content')
+    <h1>お店一覧</h1>
 
-        @foreach ($shops as $shop)
+    @foreach ($shops as $shop)
         <p>
             {{ $shop->category->name }}
             {{ $shop->name }}
             {{ $shop->address }}
         </p>
-        @endforeach
+    @endforeach
     </body>
-</html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/shops', 'App\Http\Controllers\ShopController@index');
+Route::get('/shops', 'App\Http\Controllers\ShopController@index')->name('shop.list');
 Route::get('/', function () {
     return redirect('/shops');
 });
